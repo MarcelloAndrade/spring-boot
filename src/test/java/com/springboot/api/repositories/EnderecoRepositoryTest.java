3fbf8e86b3e2770cf26183b8af27fc3fd25784c7
Marcello Andrade <marcellohfandrade@gmail.com>
2017-10-23 23:12:15 -0200

    Adicionado correção classe de testes

--- src/test/java/com/springboot/api/repositories/EnderecoRepositoryTest.java
+++ src/test/java/com/springboot/api/repositories/EnderecoRepositoryTest.java
@@ -1,74 +1,14 @@
 package com.springboot.api.repositories;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.Date;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.springboot.api.entiy.Usuario;
-import com.springboot.api.enums.EnumPerfil;
-import com.springboot.api.utils.PasswordUtil;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class EnderecoRepositoryTest {
-
-	@Autowired
-	private UsuarioRepository usuarioRepository;
 	
-	private static final String CPF = "38531201213";
-	private static final String EMAIL = "test@test";
-
-	@Before
-	public void setUp() throws Exception {
-		Usuario usuario = new Usuario();
-		usuario.setNome("Marcello Andrade");
-		usuario.setCpf(CPF);
-		usuario.setEmail(EMAIL);
-		usuario.setSenha(PasswordUtil.gerarBCrypt("senhadeteste"));
-		usuario.setDataNascimento(new Date());
-		usuario.setPerfil(EnumPerfil.ROLE_USUARIO);
-		this.usuarioRepository.save(usuario);
-	}
-	
-	@After
-    public final void tearDown() { 
-		this.usuarioRepository.deleteAll();
-	}
-
-	@Test
-	public void testBuscarPorCpf() {
-		Usuario usuario = this.usuarioRepository.findByCpf(CPF);
-		assertEquals(CPF, usuario.getCpf());
-	}
-	
-	@Test
-	public void testBuscarPorEmail() {
-		Usuario usuario = this.usuarioRepository.findByEmail(EMAIL);
-		assertEquals(EMAIL, usuario.getEmail());
-	}
-	
-	@Test
-	public void testBuscarPorCpfOuEmailOndeCpfTrue() {
-		Usuario usuario = this.usuarioRepository.findByCpfOrEmail(CPF, "");
-		assertEquals(CPF, usuario.getCpf());
-		assertEquals(EMAIL, usuario.getEmail());
-	}
-	
-	@Test
-	public void testBuscarPorCpfOuEmailOndeEmailTrue() {
-		Usuario usuario = this.usuarioRepository.findByCpfOrEmail("", EMAIL);
-		assertEquals(CPF, usuario.getCpf());
-		assertEquals(EMAIL, usuario.getEmail());
-	}
 }
 
